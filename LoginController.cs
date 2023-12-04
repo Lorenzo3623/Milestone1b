4d98ac3221d91cab2ab03b6703d13b17f32e3db2
Lorenzo3623 <126446224+Lorenzo3623@users.noreply.github.com>
2023-12-03 18:44:43 -0700

    Add files via upload

---
+++ LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Milestone1.Models;
+using Milestone1.Services;
+
+
+namespace Milestone1.Controllers
+{
+	public class LoginController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult ProcessLogin(UserModel user)
+		{
+			SecurityDAO securityDAO = new SecurityDAO();
+            if (securityDAO.InsertUser(user))
+                securityDAO.InsertUser(user);
+			return View(user);
+		}
+	}
+}
+
---
+++ UserModel.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Milestone1.Models
+{
+	public class UserModel
+	{
+		[Required]
+		[DisplayName("First Name")]
+		[StringLength(50)]
+		public string FirstName { get; set; }
+
+		[Required]
+		[DisplayName("Last Name")]
+		[StringLength(50)]
+		public string LastName { get; set; }
+
+		[Required]
+		[DisplayName("Sex")]
+		public string Sex { get; set; } // Can also be implemented as an Enum
+
+		[Required]
+		[Range(1, 150)]
+		[DisplayName("Age")]
+		public int Age { get; set; }
+
+		[Required]
+		[DisplayName("State")]
+		public string State { get; set; } // Can use a predefined list or Enum
+
+		[Required]
+		[EmailAddress]
+		[DisplayName("Email Address")]
+		public string Email { get; set; }
+
+		[Required]
+		[DisplayName("Username")]
+		[StringLength(20, MinimumLength = 4)]
+		public string Username { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[DisplayName("Password")]
+		[StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long")]
+		public string Password { get; set; }
+	}
+}
